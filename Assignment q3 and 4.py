f75d5181c97bb5bad81ce5c5286c4e52bf639089
Oluwasheuna <42254507+Oluwasheuna@users.noreply.github.com>
2018-08-14 21:26:23 +0100

    Add files via upload

---
+++ Assignment q3 and 4.py
@@ -0,0 +1,31 @@
+#to calculate the area of a trapezoid, square, cylinder, triangle
+
+
+radius = int(input("enter radius\n"))
+Ɵ = int(input("enter Ɵ\n"))
+Area  = (1 / 2) * radius * radius * Ɵ
+print (Area)
+
+
+length = int(input("enter length\n"))
+breath = int(input("enter breath\n"))
+height = int(input("enter height\n"))
+Area = ((length + breath) / 2) * height
+print (Area)
+
+length = int(input("enter length\n"))
+Area = 4 * length
+print (Area)
+
+h = int(input("enter h\n"))
+r = int(input("enter r\n"))
+Area = (2 * 3.142 * r * h) + (2 * 3.142 * r * r)
+print (Area)
+
+
+height = int(input('enter height:'))
+base = int(input('enter base:'))
+Area = (height * base) / 2
+print (Area)
+             
+         
